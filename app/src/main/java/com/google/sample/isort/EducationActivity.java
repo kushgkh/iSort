62b477effee52b6d336da42bf5940a96e1e32c06
juhipark <juhipark@student.hartnell.edu>
2018-01-21 09:42:41 -0800

    Created the education activity

---
+++ app/src/main/java/com/google/sample/isort/EducationActivity.java
@@ -0,0 +1,13 @@
+package com.google.sample.isort;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education);
+    }
+}
--- app/src/main/java/com/google/sample/isort/LoginPage.java
+++ app/src/main/java/com/google/sample/isort/LoginPage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_main);
         Button loginButton = (Button) findViewById(R.id.loginbutton);
         DataHolder.setData("True");
